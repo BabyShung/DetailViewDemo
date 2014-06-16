8a41263231e2addd0890203dec8733fc66cc6b0f
BabyShung <hao-zheng@uiowa.edu>
2014-06-16 14:48:54 -0500

    Initial Commit

---
+++ DetailViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DetailViewDemo
+//
+//  Created by Hao Zheng on 6/16/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DetailViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DetailViewDemo
+//
+//  Created by Hao Zheng on 6/16/14.
+//  Copyright (c) 2014 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
